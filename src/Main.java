a07fefca98c9332b87422d53377a9519c591fde0
Valentin Fadeev <valentinfs@yandex.ru>
2021-08-16 21:24:31 +0300

    Report added

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        long balans = 2_000_000_000;
+        int balans = 2_000_000_000;
         int amount = 500_000_000;
-        long total = balans + amount;
+        int total = balans + amount;
 
         System.out.println(total);
 
