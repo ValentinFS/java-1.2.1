2f55042a542106d56d1f85dc82f5ae77c042a109
Valentin Fadeev <valentinfs@yandex.ru>
2021-08-16 20:55:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        long balans = 2_000_000_000;
+        int amount = 500_000_000;
+        long total = balans + amount;
+
+        System.out.println(total);
+
+
+    }
+
+
+}
